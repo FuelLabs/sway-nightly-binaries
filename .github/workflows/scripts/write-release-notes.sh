c051472e0b3666b25f3d651cf4e43ddefcb70174
bing <binggh@proton.me>
2022-09-05 15:46:58 +0800

    create and use write-release-notes script (#3)

---
+++ .github/workflows/scripts/write-release-notes.sh
@@ -0,0 +1,18 @@
+touch body.md
+touch body.tmp
+git log --after="${{ steps.set-date.outputs.yesterday }} 00:00" --before="${{ steps.set-date.outputs.today }} 00:00" --oneline >> body.tmp
+echo "Generating release notes from repo: ${1}"
+
+while read -r line; do
+	  commit=$(echo "$line" | cut -d ' ' -f1)
+	  pr=$(echo "$line" | cut -d '#' -f2 | rev | cut -c2- | rev)
+	  description=$(echo "$line" | cut -d ' ' -f2- | rev | cut -d ' ' -f2- | rev)
+
+	  commit_url="https://github.com/FuelLabs/${1}/commit/${commit}"
+	  pr_url="https://github.com/FuelLabs/${1}/pull/${pr}"
+
+	  echo "- [${commit}](${commit_url}) ${description} [#${pr}](${pr_url})" >> body.md
+done <body.tmp
+
+echo "" >> body.md
+rm body.tmp
